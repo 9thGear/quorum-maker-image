5c59295943fb37fd5077fe0804e63bf8c47a7fb4
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-01-10 15:44:47 -0600

    Add packages needed for ZSL

--- lib/install_baseimage.sh
+++ lib/install_baseimage.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-#This is for base tag of image syneblock/quorum-master:base
 apt-get update
-apt-get install -y libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev
+
+# From https://github.com/9thGear/quorum-deployment/wiki/Setup-Quorum-with-ZSL-From-Scratch
 apt-get install -y build-essential
-apt-get install software-properties-common python-software-properties -y
-apt-get install -y wget
+apt-get install -y cmake
+apt-get install -y git
+apt-get install -y libgmp3-dev
+apt-get install -y libprocps4-dev
+apt-get install -y python-markdown
+apt-get install -y libboost-all-dev libssl-dev libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev
+apt-get install -y git
 apt-get install -y curl
+apt-get install -y wget
+
+# From Quorum Maker
+apt-get install -y software-properties-common python-software-properties
 apt-get install -y jq
 apt-get install -y psmisc
 apt-get install -y iputils-ping
-add-apt-repository ppa:ethereum/ethereum -y
+
+add-apt-repository -y ppa:ethereum/ethereum
 apt-get update
 apt-get install solc -y
-apt-get install bsdmainutils
+
+apt-get install -y bsdmainutils
 apt-get install -y openjdk-8-jdk
-apt-get -y install maven
-apt-get -y install net-tools
+apt-get install -y maven
+apt-get install -y net-tools
