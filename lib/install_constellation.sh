0e9ea0ce0935d7d69e0df42c72c6d2e083558c16
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-01-10 15:45:27 -0600

    Update golang version to 1.7.6

--- lib/install_constellation.sh
+++ lib/install_constellation.sh
@@ -5,4 +5,4 @@ tar -xf constellation-0.3.2-ubuntu1604.tar.xz
 cp constellation-0.3.2-ubuntu1604/constellation-node /usr/bin/.
 cd ..
 
-rm -rf go1.10.linux-amd64.tar.gz
+rm -rf go1.7.6.linux-amd64.tar.gz
