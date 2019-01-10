0ee022ac51c0eb8426b32509a63a4823da0dd2eb
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-01-10 15:44:22 -0600

    Update golang version to use 1.7.6

--- lib/common.sh
+++ lib/common.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 
 RED=$'\033[0;31m'
 CYAN=$'\e[1;96m'
 COLOR_END=$'\033[0m' # No Color
 
 function chownDir {
-    docker run -it --rm -v $(pwd)/$1:/$1 -w / -e QUID=$UID -e QGID=$(id -g $USER) golang:1.10.2 /bin/sh -c 'chown -R $QUID:$QGID '$1
+    docker run -it --rm -v $(pwd)/$1:/$1 -w / -e QUID=$UID -e QGID=$(id -g $USER) golang:1.7.6 /bin/sh -c 'chown -R $QUID:$QGID '$1
 }
