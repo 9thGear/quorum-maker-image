612e0503141dbdc53d5e4c52c8e23229613917b7
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-01-14 10:32:06 -0600

    Update quorum make command to utilize custom go Docker image

--- lib/install_quorum.sh
+++ lib/install_quorum.sh
@@ -9,7 +9,7 @@ rm -rf quorum
 
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://github.com/jpmorganchase/quorum.git
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git -w /git/quorum alpine/git checkout $quorum_version
-docker run -it --rm -v $(pwd)/quorum:/quorum -w /quorum golang:1.7.6 make all
+docker run -it --rm -v $(pwd)/quorum:/quorum -w /quorum custom-go:latest make all
 
 ## Change the owneship of quorum directory
 chownDir 'quorum'
