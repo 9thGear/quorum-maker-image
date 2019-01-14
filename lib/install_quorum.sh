245e43f36103e02a718848c5b36f6da8cd21f9bc
Jay Payne <letterj@gmail.com>
2019-01-14 13:26:04 -0600

    zsl build changes

--- lib/install_quorum.sh
+++ lib/install_quorum.sh
@@ -9,7 +9,8 @@ rm -rf quorum
 
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://github.com/jpmorganchase/quorum.git
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git -w /git/quorum alpine/git checkout $quorum_version
-docker run -it --rm -v $(pwd)/quorum:/quorum -w /quorum custom-go:latest make all
+# docker run -it --rm -v $(pwd)/quorum:/quorum -w /quorum custom-go:latest make all
+docker run -it --rm -v $(pwd)/quorum:/quorum -w /quorum golang:1.7.6 /bin/bash -c "apt-get update && apt-get install -y build-essential cmake git libgmp3-dev libprocps3-dev python-markdown libboost-all-dev libssl-dev && make all"
 
 ## Change the owneship of quorum directory
 chownDir 'quorum'
